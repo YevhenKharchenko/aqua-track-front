1409b8b927b64d247b6d1647005444a87131d50b
KateMassa <catherineorlova27@gmail.com>
2024-07-12 21:00:56 +0200

    add changes

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,22 +1,18 @@
 //import currentIntake
 //import dailyNormValue
 import css from './WaterProgressBar.module.css';
-import { Line } from 'react-progressbar.js';
 
-const WaterProgressBar = ({ currentIntake, dailyNormValue }) => {
+const WaterProgressBar = ({ currentIntake = 800, dailyNormValue = 2000 }) => {
+  // Suppose we get the current consumption and daily rate from props or context
+
   const progress = (currentIntake / dailyNormValue) * 100;
-
-  const options = {
-    color: '#9BE1A0',
-    strokeWidth: 9,
-    trailColor: '#e0e0e0',
-    trailWidth: 9,
-  };
 
   return (
     <div className={css.progressBarContainer}>
       <h3 className={css.progressTitle}>Today</h3>
-      <Line progress={progress / 100} options={options} initialAnimate={true} />
+      <div className={css.progressBar}>
+        <div className={css.progressBarFill} style={{ width: `${progress}%` }}></div>
+      </div>
       <ul className={css.percentageList}>
         <li>0%</li>
         <li>50%</li>
