8dd455525529b8c0ef94faee94eb17006de93da5
OlehKotov <mail.oleg456@gmail.com>
2024-07-13 23:45:32 +0300

    Update 1440 css

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -1,10 +1,10 @@
 import css from "./AdvantagesSection.module.css";
-// import Rectangle19 from '../../assets/images/Rectangle19.png'; 
+ 
 
 const AdvantagesSection = () => {
   return (
     <div className={css.container}>
-        {/* <img src={Rectangle19} alt="Description of image" /> */}
+        {/* картинка */}
     </div>
   )
 }
