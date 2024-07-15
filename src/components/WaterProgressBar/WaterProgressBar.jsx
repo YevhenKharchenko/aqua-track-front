324f9efe50d258d677a688c577363ba0cc71e61c
KateMassa <catherineorlova27@gmail.com>
2024-07-15 18:08:39 +0200

    update water Progress bar

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -10,14 +10,38 @@ const WaterProgressBar = ({ currentIntake = 1500, dailyNormValue = 2000 }) => {
 
   const progress = (currentIntake / dailyNormValue) * 100;
 
+  const renderPercentage = () => {
+    const percentages = [];
+    const result = [];
+
+    for (let i = 0; i <= 100; i += 10) {
+      if (!percentages.includes(i) && i < progress && i > progress - 10) {
+        result.push(progress.toFixed(0));
+      }
+      if (percentages.includes(i)) {
+        result.push(i);
+      }
+    }
+    return result.map((percentage, index) => (
+      <li
+        key={index}
+        className={parseInt(percentage, 10) === parseInt(progress, 10) ? css.exactProgressText : ''}
+      >
+        {percentage}%
+      </li>
+    ));
+  };
+
   return (
     <div className={css.progressBarContainer}>
       <h3 className={css.progressTitle}>Today</h3>
+
       <div className={css.progressBar}>
         <div className={css.progressBarFill} style={{ width: `${progress}%` }}>
-          <div className={css.progressIndicator} />
+          <div className={css.progressIndicator}></div>
         </div>
       </div>
+      <ul className={css.actualPercentage}>{renderPercentage()}</ul>
       <ul className={css.percentageList}>
         <li>0%</li>
         <li>50%</li>
