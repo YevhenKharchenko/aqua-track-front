77448036b5e611a41cd70cb451b1ce9408c16bb1
YevhenKharchenko <evgenvalerych@gmail.com>
2024-07-16 18:52:23 +0300

    update icons path

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,5 +1,6 @@
 import { useRef, useState, useEffect, useCallback } from 'react';
 import { useModal } from '../../hooks/useModal.jsx';
+import { icons as sprite } from '../../assets/icons/index.js';
 import UserBar from '../UserBar/UserBar';
 import UserBarPopover from '../UserBarPopover/UserBarPopover';
 import LogOutModal from '../LogOutModal/LogOutModal.jsx';
@@ -71,19 +72,13 @@ const UserPanel = () => {
             data-setting-button
           >
             <svg width="16" height="16">
-              <use
-                className={css.iconSettings}
-                href="./src/assets/icons/sprite.svg#icon-settings-16x16"
-              ></use>
+              <use className={css.iconSettings} xlinkHref={`${sprite}#icon-settings-16x16`}></use>
             </svg>
             Setting
           </button>
           <button className={css.logoutBtn} type="button" onClick={openModal} data-logout-button>
             <svg width="16" height="16">
-              <use
-                className={css.iconLogout}
-                href="./src/assets/icons/sprite.svg#icon-log-out-16x16"
-              ></use>
+              <use className={css.iconLogout} xlinkHref={`${sprite}#icon-log-out-16x16`}></use>
             </svg>
             Log out
           </button>
