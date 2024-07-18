24266aade43dc315b0c6fa1c6a889943e1332901
YevhenKharchenko <evgenvalerych@gmail.com>
2024-07-18 13:03:16 +0300

    update styles

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,13 +1,13 @@
 import UserPanel from '../UserPanel/UserPanel';
 import { DailyInfo } from '../DailyInfo/DailyInfo';
-// import MonthInfo from '../MonthInfo/MonthInfo';
+import MonthInfo from '../MonthInfo/MonthInfo';
 import css from './WaterDetailedInfo.module.css';
 const WaterDetailedInfo = () => {
   return (
     <div className={css.waterDetailInfo}>
       <UserPanel />
       <DailyInfo />
-      {/* <MonthInfo /> */}
+      <MonthInfo />
     </div>
   );
 };
