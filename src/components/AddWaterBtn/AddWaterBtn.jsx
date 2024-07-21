f5ecf31fe2b5f7011f8c2790ab3c56d7a50f5958
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-07-22 00:23:04 +0200

    чматсптспь

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -3,8 +3,11 @@ import { useModal } from '../../hooks/useModal.jsx';
 import { useCallback } from 'react';
 import WaterModal from '../WaterModal/WaterModal';
 import css from './AddWaterBtn.module.css';
+import { useTranslation } from 'react-i18next'; // імпорт хука useTranslation
+import '../../i18n'; // Імпортуйте конфігурацію i18next
 
 const AddWaterBtn = ({ className, icon, iconClassName }) => {
+  const { t } = useTranslation(); // використання хука для перекладу
   const setModal = useModal();
 
   const closeModal = useCallback(() => {
@@ -21,9 +24,10 @@ const AddWaterBtn = ({ className, icon, iconClassName }) => {
         <svg className={clsx(css.icon, iconClassName)}>
           <use href={icon}></use>
         </svg>
-        Add water
+        {t('addWater')} {/* Замініть текст на функцію перекладу */}
       </button>
     </>
   );
 };
+
 export default AddWaterBtn;
---
+++ src/i18n.js
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend'; // Використовуйте i18next-http-backend для завантаження файлів
+import LanguageDetector from 'i18next-browser-languagedetector'; // Додайте детектор мови
+
+i18n
+  .use(Backend) // Використовуйте http-backend для завантаження файлів
+  .use(LanguageDetector) // Додайте детектор мови
+  .use(initReactI18next)
+  .init({
+    lng: 'en', // Мова за замовчуванням
+    fallbackLng: 'en', // Мова для резервного використання
+    debug: true, // Виводити помилки у консоль
+    backend: {
+      // Шляхи до файлів перекладу
+      loadPath: '/locales/{{lng}}/translation.json'
+    },
+    interpolation: {
+      escapeValue: false // React вже обробляє escape HTML
+    },
+    react: {
+      useSuspense: false // Використовувати Suspense для завантаження перекладів
+    }
+  });
+
+export default i18n;
