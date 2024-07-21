3b4e75317ddeb8329601613456a3ce808a36270c
Merylly <142231027+Merylly@users.noreply.github.com>
2024-07-21 16:21:32 +0300

    fix validation

--- src/schemas/UserSettingsFormSchema.js
+++ src/schemas/UserSettingsFormSchema.js
@@ -5,7 +5,7 @@ export const userSettingsFormSchema = yup.object().shape({
     .string()
     .required('Please enter your name!')
     .min(2, 'Name must be at least 2 characters long.')
-    .max(50, 'Name must be less than 50 characters long.'),
+    .max(20, 'Name must be less than 20 characters long.'),
   email: yup
     .string()
     .required('Please enter your email address.')
@@ -16,15 +16,15 @@ export const userSettingsFormSchema = yup.object().shape({
     .number()
     .typeError('Weight must be a number.')
     .min(10, 'Weight must be 10 or more.')
-    .max(300, 'Weight must be less than or equal to 300 kg.'),
+    .max(180, 'Weight must be less than or equal to 180 kg.'),
   sportTime: yup
     .number()
     .typeError('Active sport time must be a number.')
     .min(0, 'Active sport time must be a positive number.')
-    .max(12, 'Active sport time must be less than or equal to 12 hours.'),
+    .max(14, 'Active sport time must be less than or equal to 14 hours.'),
   waterNorma: yup
     .number()
     .typeError('Daily water consumption must be a number.')
     .positive('Daily water consumption must be a positive number.')
-    .max(10000, 'Daily water consumption must be less than or equal to 10000 ml.'),
+    .max(15, 'Daily water consumption must be less than or equal to 15 l.'),
 });
