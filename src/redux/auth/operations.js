dc4447935467e1671b1af08563e5fd03ba721132
YevhenKharchenko <evgenvalerych@gmail.com>
2024-07-23 12:03:51 +0300

    update auth

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -55,6 +55,8 @@ export const registerUser = createAsyncThunk(
     try {
       const res = await axios.post('/users/register', { email, password });
 
+      setAuthHeader(res.data.data.accessToken);
+
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -66,7 +68,7 @@ export const loginUser = createAsyncThunk('auth/login', async ({ email, password
   try {
     const res = await axios.post('/users/login', { email, password });
 
-    setAuthHeader(res.data.accessToken);
+    setAuthHeader(res.data.data.accessToken);
 
     return res.data;
   } catch (error) {
